bbce2a22e31aa360d79a9ea19668ff74e868afbf
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-11 13:03:21 +0300

    add repository

---
+++ src/main/java/logarea/demo/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package logarea.demo.repository;
+
+import logarea.demo.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
---
+++ src/main/java/logarea/demo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package logarea.demo.repository;
+
+import logarea.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String name);
+}
